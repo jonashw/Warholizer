66e619f4d2d9edc5a1373408c2067c543792849c
Jon <2jonwilson@gmail.com>
2024-04-25 23:23:37 -0700

    extract routeConfig for easier maintenance

--- src/main.tsx
+++ src/main.tsx
@@ -1,65 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css';
 import './App.css';
 import {
   createBrowserRouter,
-  Outlet,
   RouterProvider,
 } from "react-router-dom";
-import Gallery from './Gallery.tsx';
-import TilingPatternGallery from './TilingPatternGallery.tsx';
-import OperationEditorDemo from './OperationEditorDemo.tsx';
+import { routeConfig } from './routeConfig.tsx';
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element: (
-      <div>
-        <div className="nav nav-pills nav-fill mb-3" style={{
-          zIndex:'1000'
-        }}>{[
-          {href:'/',label:'Warholizer'},
-          {href:'/gallery',label:'Gallery'},
-          {href:'/tiling-patterns',label:'Tiling Patterns'},
-          {href:'/edit',label:'Operation Editor Demo'}
-        ].map(route => (
-          <li className="nav-item" key={route.label}>
-            <a 
-              className={"nav-link " + (window.location.pathname === route.href ? "active" : "")}
-              href={route.href}
-            >{route.label}</a>
-          </li>
-        ))}</div>
-        <div>
-          <Outlet/>
-        </div>
-      </div>
-    ),
-    children: [
-      {
-        path: '',
-        element: <App />,
-      },
-      {
-        path: 'tiling-patterns',
-        element: <TilingPatternGallery/>
-      },
-      {
-        path: 'gallery',
-        element: <Gallery/>
-      },
-      {
-        path: 'edit',
-        element: <OperationEditorDemo/>
-      }
-    ]
-  }
-]);
+const router = createBrowserRouter(routeConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ src/routeConfig.tsx
@@ -0,0 +1,51 @@
+import { Outlet, RouteObject } from "react-router-dom"
+import App from "./App";
+import TilingPatternGallery from "./TilingPatternGallery";
+import Gallery from "./Gallery";
+import OperationEditorDemo from "./OperationEditorDemo";
+
+export const routeConfig: RouteObject[] = [
+  {
+    path:'/',
+    element: (
+      <div>
+        <div className="nav nav-pills nav-fill mb-3" style={{
+          zIndex:'1000'
+        }}>{[
+          {href:'/',label:'Warholizer'},
+          {href:'/gallery',label:'Gallery'},
+          {href:'/tiling-patterns',label:'Tiling Patterns'},
+          {href:'/edit',label:'Operation Editor Demo'}
+        ].map(route => (
+          <li className="nav-item" key={route.label}>
+            <a 
+              className={"nav-link " + (window.location.pathname === route.href ? "active" : "")}
+              href={route.href}
+            >{route.label}</a>
+          </li>
+        ))}</div>
+        <div>
+          <Outlet/>
+        </div>
+      </div>
+    ),
+    children: [
+      {
+        path: '',
+        element: <App />,
+      },
+      {
+        path: 'tiling-patterns',
+        element: <TilingPatternGallery/>
+      },
+      {
+        path: 'gallery',
+        element: <Gallery/>
+      },
+      {
+        path: 'edit',
+        element: <OperationEditorDemo/>
+      }
+    ]
+  }
+];
