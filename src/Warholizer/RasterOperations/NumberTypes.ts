cc76b6ccbe1c5644576e8136261803d2984b51bb
Jon <2jonwilson@gmail.com>
2024-04-27 20:15:26 -0700

    PositiveNumber

--- src/Warholizer/RasterOperations/NumberTypes.ts
+++ src/Warholizer/RasterOperations/NumberTypes.ts
@@ -9,11 +9,21 @@ export type Byte = IntRange<0,256>;
 export type Percentage = IntRange<0,101>;
 export type Angle = IntRange<0,361>;
 
+type NonNegativeInteger<T extends number> =
+    number extends T 
+        ? never 
+        : `${T}` extends `-${string}` | `${string}.${string}`
+            ? never 
+            : T;
+
+export type PositiveNumber = NonNegativeInteger<number>;
 const clamp = (min: number, input: number, max: number): number =>
     Math.max(min,Math.min(max, input));
 
 export const byte = <Byte>(input: number): Byte =>
     clamp(0,input,255) as Byte;
+export const positiveNumber = <PositiveNumber>(input: number): PositiveNumber =>
+    clamp(0,input,Infinity) as PositiveNumber;
 export const percentage = <Percentage>(input: number): Percentage =>
     clamp(0,input,100) as Percentage;
 export const angle = <Angle>(input: number): Angle =>
