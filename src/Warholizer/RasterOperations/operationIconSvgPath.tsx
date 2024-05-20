1d80106ae441324257eb2ac15fbc7a92cd22b3ca
Jon Wilson <Jon.Wilson@treasurer.ca.gov>
2024-05-20 09:40:19 -0700

    document source of svg extraction

--- src/Warholizer/RasterOperations/operationIconSvgPath.tsx
+++ src/Warholizer/RasterOperations/operationIconSvgPath.tsx
@@ -5,6 +5,7 @@ import { OperationIcon } from "./OperationIcon";
 const parser = new DOMParser();
 
 export const operationIconSvgPath = (op: PureRasterOperation) => {
+    //source: https://stackoverflow.com/a/68889169/943730
     const iconString = ReactDOMServer.renderToString(<OperationIcon op={op} className="" />);
     const svgDoc = parser.parseFromString(iconString, 'image/svg+xml');
     const iconPath = svgDoc.querySelector('path')?.getAttribute('d') as string;
