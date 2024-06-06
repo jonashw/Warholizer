774a0a00b1c47c7f6c6df04f74b70fd31c96074c
Jon Wilson <Jon.Wilson@treasurer.ca.gov>
2024-06-06 14:14:22 -0700

    fix default fill color (should be hex)

--- src/sampleOperations.tsx
+++ src/sampleOperations.tsx
@@ -4,7 +4,7 @@ import { angle, byte, positiveNumber } from './Warholizer/RasterOperations/Numbe
 
 export const sampleOperations: PureRasterOperation[] = ([
     { "type": "noop"},
-    { "type": "fill", color:"green"},
+    { "type": "fill", color:"#3333ff"},
     { "type": "split", dimension:"x",amount:50},
     { "type": "void"},
     { "type": "stack", blendingMode: "multiply" },
