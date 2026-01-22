3fb0a8c7cf32b115c83cba51619d75f295c6db0d
Jon Wilson <jon.wilson@dhcs.ca.gov>
2026-01-22 10:52:28 -0800

    fix build

--- src/defaultApplicator.tsx
+++ src/defaultApplicator.tsx
@@ -1,4 +1,4 @@
 import { PureRasterApplicator } from './Warholizer/RasterOperations/PureRasterApplicator';
 
 
-export const defaultApplicator: PureRasterApplicator = { "type": "pipe", ops: [] };
+export const defaultApplicator: PureRasterApplicator = { "type": "pipe", ops: [], enabled: true };
